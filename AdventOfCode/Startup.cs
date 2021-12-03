36c1e8a4f3db66b975aca2369f3423384c358dff
Peterjan Peeters <peterjan.peeters@gmail.com>
2021-12-03 16:46:52 +0100

    Added stopwatch to time solutions.

--- AdventOfCode/Startup.cs
+++ AdventOfCode/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using AdventOfCode.Helpers;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -28,11 +29,18 @@ namespace AdventOfCode
 
             Console.WriteLine($"Solving puzzle for year {year} day {day}:");
 
+            var sw = new Stopwatch();
+            sw.Start();
+
             var resultPart1 = puzzle.SolvePart1();
             Console.WriteLine($"Part1 result: {resultPart1}");
-            
+            Console.WriteLine($"Compute time part 1: {sw.Elapsed:c}");
+            sw.Restart();
+
             var resultPart2 = puzzle.SolvePart2();
             Console.WriteLine($"Part2 result: {resultPart2}");
+            Console.WriteLine($"Compute time part 2: {sw.Elapsed:c}");
+            sw.Stop();
         }
     }
 }
