d18831b3598faac8ee32020ee79aef02842d9d76
Peterjan Peeters <peterjan.peeters@gmail.com>
2021-12-11 14:48:38 +0100

    Added preparation for Day 12.

---
+++ AdventOfCode/2021/Day12.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using AdventOfCode;
+using AdventOfCode.Helpers;
+using Microsoft.Extensions.Logging;
+
+namespace AdventOfCode2021.Day12
+{
+    // https://adventofcode.com/2021/day/12
+    public class Day12 : BaseDay
+    {
+        protected override string Year => "2021";
+        protected override string Day => "12";
+
+        public IInputManager InputManager { get; }
+        public ILogger Logger { get; }
+
+        public Day12(IInputManager inputManager, ILogger logger)
+        {
+            InputManager = inputManager;
+            Logger = logger;
+            Init(Year, Day);
+        }
+
+        private void Init(string year, string day)
+        {
+            var rawInputs = InputManager.GetInputNumbers(year, day);
+
+        }
+
+        // Part1 result: ?
+        public override object SolvePart1()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Part2 result: ?
+        public override object SolvePart2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
